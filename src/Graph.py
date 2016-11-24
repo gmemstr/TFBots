0c47c55d2705bb38f2439026878b50aace350aaf
Gabriel Simmer <bladesimmer@gmail.com>
2016-11-23 22:04:08 -0800

    Graph displays price in keys, USD and metal.
    Tweaked a few things, svgs are now ignored in .gitignore since they are generated upon running Calculate.py anyways. Also fixed iterating through the cache for Graph.py, should properly iterate through data with InitGraphing(). Need to clean up the source a bit before final master commit, that should come soon.
    
    Next up, work on index template so it's not quite so boring.

--- src/Graph.py
+++ src/Graph.py
@@ -1,24 +1,26 @@
 import matplotlib.pyplot as plt
 import json
 import sys
+import time
 import Cache
 
 
 def InitGraphing():
-    cache = Cache.Fetch("cache", "all")
-    for site in cache[time]:
-        GraphStocks(site)
+    cache = Cache.Fetch("json/cache.json", "all")
+    for site in cache[time.strftime("%x")]:
+            GraphStocks(site)
 
 
 def GraphStocks(site):
-
-    cache = Cache.Fetch("cache", "all")
-    keys = Cache.Fetch("prices")["keys"]
+    cache = Cache.Fetch("json/cache.json", "all")
+    keys = Cache.Fetch("json/prices.json")["keys"]
+    metal = Cache.Fetch("json/prices.json")["metal"]
     plt.figure(1, figsize=(16, 6.67), dpi=75)
     plt.clf()
     history = []
     prices = []
     i = 0
+
     for time in sorted(cache)[-10:]:
         print("Result found at " + time)
         history.append(time)
@@ -27,7 +29,8 @@ def GraphStocks(site):
 
     plt.ylabel('Value in refined metal')
     plt.title(site + " ref \nTotal: " + str(prices[i - 1]) + " refined " +
-              str(prices[i - 1] / keys) + " keys")
+              str(round(prices[i - 1] / keys, 2)) + " keys $" +
+              str(round(prices[i - 1] / metal, 2)) + " USD")
     x = range(i)
     plt.xticks(x, history)
     plt.plot(x, prices, "g")
